d7287a28ca1c7a668647538f63d14145a1d90d5a
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-09-22 01:38:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,4 +8,7 @@ def turn_count(board)
   return counter
 end
 
-current_player(board) = turn_count(board) % 2 == 0 ? "X" : "O"
+def current_player(board)
+  turn_count(board).even? == true ? "X" : "O"
+end
+ 

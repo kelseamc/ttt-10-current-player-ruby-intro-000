5277759d73b76daccfd89868ae5d832e471095c2
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-09-22 01:17:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board)
-  counter = 0
   board.each do |position|
+    counter = 0
     if (position == "X" || position == "O")
-    counter += 1
+      counter += 1
+    end
   end
+  return counter
 end
